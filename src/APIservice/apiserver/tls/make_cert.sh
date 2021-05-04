786cb58d49d954addc653c8a7da558ea12f5c4c7
smlu <smlu@s5.net>
2021-05-04 14:26:13 +0200

    Update test TLS certificates & script

--- src/APIservice/apiserver/tls/make_cert.sh
+++ src/APIservice/apiserver/tls/make_cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-openssl req -new -x509 -nodes -days 356 -sha384 -subj "/O=ZeroPass Port/OU=Port Server/CN=Port Server" -key server_key.pem -out "port_server_new.cer"
+openssl req -new -x509 -nodes -days 65535 -sha384 -subj "/O=ZeroPass/OU=Port Server/CN=Port Server" -key server_key.pem -out "port_server_new.cer"
 
 # der encoding
 openssl x509 -outform der -in "port_server_new.cer" -out "port_server_new.der"
