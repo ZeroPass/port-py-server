2e8a63bbf5dc5a8eb3080d60db0e1988439e8b2a
smlu <smlu@s5.net>
2021-07-02 15:53:38 +0200

    [proto] Fix UserId init value size && Add __str__ & __repr__ overloads

--- src/port/proto/user.py
+++ src/port/proto/user.py
@@ -1,5 +1,4 @@
-import base64, hashlib
-from pymrtd.pki.keys import AAPublicKey
+import base64
 from typing import cast
 
 class UserIdError(Exception):
@@ -12,7 +11,7 @@ class UserId(bytes):
 
     def __new__(cls, userId: bytes) -> "UserId":
         if not isinstance(userId, bytes) or \
-            len(userId) < UserId.max_size:
+            len(userId) > UserId.max_size:
             raise UserIdError("Invalid userId data")
         return cast(UserId, super().__new__(cls, userId))  # type: ignore  # https://github.com/python/typeshed/issues/2630  # noqa: E501
 
@@ -23,3 +22,12 @@ class UserId(bytes):
 
     def toBase64(self):
         return str(base64.b64encode(self), 'ascii')
+
+    def __str__(self) -> str:
+        try:
+            return self.decode("utf-8")
+        except:
+            return self.hex()
+
+    def __repr__ (self) -> str:
+        return "UserId({})".format(self.__str__())
