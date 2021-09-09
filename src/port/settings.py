e6b2375ef1cf0557600d606f42709c1b07697243
smlu <smlu@s5.net>
2021-09-09 17:28:38 +0200

    [port] Refactor settings.py

--- src/port/settings.py
+++ src/port/settings.py
@@ -3,23 +3,29 @@ from typing import NamedTuple
 
 
 class DbConfig(NamedTuple):
-    dialect: str #database dialect, e.g.: postgresql, mysql ...
-    url: str
-    user: str
-    pwd: str
-    db: str
+    dialect: str
+    url: str      = ''
+    name: str     = '' # database name
+    user: str     = ''
+    password: str = ''
+
+class MrtdPkd(NamedTuple):
+    path: Path
+    allow_self_issued_csca: bool = True # allow selfsigned CSCA certificate when loading CSCAs from path
+
+class HttpServerConfig(NamedTuple):
+    host: str
+    port: int
+    timeout_keep_alive: int  = 10 # close 'Keep-Alive' connection after response if no data is received for n seconds
+    log_level: Optional[str] = None
+    tls_cert: Optional[Path] = None
+    tls_key: Optional[Path]  = None
 
 class ServerConfig(NamedTuple):
-    host: str
-    port: int
-    ssl_ctx: ssl.SSLContext
-
-class WebAppConfig(NamedTuple):
-    host: str
-    port: int
-
-class Config(NamedTuple):
     database: DbConfig
-    api_server: ServerConfig
-    web_app: WebAppConfig
-    challenge_ttl: int
+    api: Optional[HttpServerConfig] = None
+    challenge_ttl: int              = 600 # 10 minutes
+    job_interval: int               = 3600  # 1 hour, an interval at which server does maintenance job and other tasks. (e.g.: delete expired proto challenges from DB)
+    log_level: str                  = 'verbose'
+    mrtd_pkd: Optional[MrtdPkd]     = None # MRTD certificate folder to load into database when server starts
+
