91a4f65b4aa4c960348e77b0752801c6532a6dee
Crt Vavros - smlu <smlu@s5.net>
2022-03-17 17:58:55 +0100

    Change && Update dependencies
    Python 3.9.11 is now required min version.
    Update github tests action

--- setup.py
+++ setup.py
@@ -20,30 +20,29 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
-    python_requires=">=3.9",
+    python_requires=">=3.9.11",
     install_requires=[
         'coloredlogs>=15.0.1',
-        'cryptography>=3.4.7',
         'json-rpc>=1.13.0',
         'ldif3>=3.2.2',
-        'orjson>=3.6.3',
-        'pycountry>=20.7.3',
-        'pymrtd>=0.6.3',
-        'pywin32>=301;platform_system=="Windows"',
-        'sqlalchemy>=1.4.23',
-        'starlette>=0.16.0',
-        'uvicorn[standard]>=0.15.0'
+        'orjson>=3.6.7',
+        'pycountry>=22.3.5',
+        'pymrtd>=0.6.4',
+        'pywin32>=303;platform_system=="Windows"',
+        'sqlalchemy>=1.4.32',
+        'starlette>=0.19.0',
+        'uvicorn[standard]>=0.17.6'
     ],
     extras_require={
         'examples' : [
-            'PyYAML>=5.4.1 ',
-            'requests>=2.26.0'
+            'PyYAML>=6.0',
+            'requests>=2.27.1'
         ],
-        'mysql' : ['mysqlclient>=2.0.3'], # for using MySQL DB
-        'postgresql' : ['psycopg2>=2.9.1'], # for using PostgreSQL DB
+        'mysql' : ['mysqlclient>=2.1.0'], # for using MySQL DB
+        'postgresql' : ['psycopg2>=2.9.3'], # for using PostgreSQL DB
         'sqlite' : ['sqlcipher3>=0.4.5'], # for using SQLite
         'tests': [
-            'pytest>=6.2.4',
+            'pytest>=7.1.0',
             'pytest-depends>=1.0.1',
             'pytest-datafiles>=2.0.0'
         ],
